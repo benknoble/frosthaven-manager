f9ba4c1ef39bbc9e004c2196a543142897f4b939
D. Ben Knoble <ben.knoble+github@gmail.com>
2022-04-07 16:52:38 -0400

    make loot-picker receive @loot-deck
    This means it is no longer "derived," so can be used as-is for further
    game state (_e.g._, updating it by removing cards as they are drawn).
    
    The downside is increased complexity: obs-observe! registers arbitrary
    callbacks, which we used to update the value of the @loot-deck. But we
    have to make sure the observed value is still referenced; _e.g._, the
    @deck value from the old code was not a good target for obs-observe!
    because changes to it (via @n) somehow disappeared (possibly because it
    was garbage-collected?). Additionally, we need to make sure that once we
    are done building the @loot-deck, we don't continue to modify it by
    modifying the observables generated by make-cards-picker! or by
    modifying @random-item?. The good news is the only objects that retain
    references to these things do not provide access to them; the bad news
    is there is no way to clear the observation call-backs to entirely
    prevent it.
    
    As long as it is impossible to re-visit the loot-deck screen once
    game-play has started, we should be OK.

--- loot.rkt
+++ loot.rkt
@@ -1,21 +1,24 @@
 #lang racket
 
 (provide (contract-out
-           [loot-picker (-> (values (obs/c (listof (or/c money?
-                                                         material?
-                                                         herb?
-                                                         random-item?)))
-                                    (is-a?/c view<%>)))]))
+           [loot-picker (-> (obs/c (listof (or/c money? material? herb? random-item?)))
+                            (is-a?/c view<%>))]))
 
 (require racket/gui/easy
          racket/gui/easy/operator
          racket/gui/easy/contract
          "defns.rkt")
 
-(define (loot-picker)
-  (define (make-cards-picker label max-cards deck)
+(define (loot-picker @loot-deck)
+  (define (make-cards-picker! label max-cards deck in-deck?)
     (define/obs @n 0)
-    (define @deck (~> @n (λ (n) (take (shuffle deck) n))))
+    (obs-observe! @n
+      (λ (amount)
+        (<~ @loot-deck
+            (λ (old-loot-deck)
+              (shuffle
+                (append (filter-not in-deck? old-loot-deck)
+                        (take (shuffle deck) amount)))))))
     (define view (hpanel (button "-" (thunk (if (obs-peek (~> @n zero?))
                                               (void)
                                               (<~ @n sub1))))
@@ -23,48 +26,51 @@
                          (button "+" (thunk (if (>= (obs-peek @n) max-cards)
                                               (void)
                                               (<~ @n add1))))))
-    (values @deck view))
-  (define-values (@money-deck money-view)
-    (make-cards-picker "Money Cards: " max-money-cards money-deck))
-  (define material-@d+v
+    view)
+  (define money-view
+    (make-cards-picker! "Money Cards: " max-money-cards money-deck money?))
+  (define material-views
     (for/list ([m (in-list material-kinds)])
-      (define-values (@d v) (make-cards-picker (~a m " Cards: ") max-material-cards
-                                               (hash-ref material-decks m)))
-      (cons @d v)))
-  (define herb-@d+v
+      (make-cards-picker! (~a m " Cards: ")
+                          max-material-cards
+                          (hash-ref material-decks m)
+                          (match-lambda
+                            [(material (== m) _) #t]
+                            [_ #f]))))
+  (define herb-views
     (for/list ([h (in-list herb-kinds)])
-      (define-values (@d v) (make-cards-picker (~a h " Cards: ") max-herb-cards
-                                               (hash-ref herb-decks h)))
-      (cons @d v)))
+      (make-cards-picker! (~a h " Cards: ")
+                          max-herb-cards
+                          (hash-ref herb-decks h)
+                          (match-lambda
+                            [(herb (== h)) #t]
+                            [_ #f]))))
   (define/obs @random-item? #f)
+  (obs-observe! @random-item?
+    (λ (rand-item?)
+      (<~ @loot-deck
+          (λ (old-loot-deck)
+            (define deck-sans-random
+              (filter-not random-item? old-loot-deck))
+            (if rand-item?
+              (shuffle
+                (cons random-item deck-sans-random))
+              deck-sans-random)))))
   (define random-item-view
     (checkbox #:label "Random Item Card?"
               (λ:= @random-item? identity)))
-  (define @loot-deck
-    (apply obs-combine
-           (λ (money rand-item? . mats-and-herbs)
-             (shuffle (apply append
-                             (if rand-item? (list random-item) empty)
-                             money
-                             mats-and-herbs)))
-           @money-deck
-           @random-item?
-           (append (map car material-@d+v)
-                   (map car herb-@d+v))))
   (define loot-view
     (apply vpanel
            random-item-view
            money-view
-           (append (map cdr material-@d+v)
-                   (map cdr herb-@d+v))))
-  (values @loot-deck loot-view))
+           (append material-views herb-views)))
+  loot-view)
 
 (module+ main
-  (define-values (@loot-deck loot-view)
-    (loot-picker))
+  (define/obs @loot-deck empty)
   (void (render (window
                   (hpanel
-                    loot-view
+                    (loot-picker @loot-deck)
                     (table '("Card")
                            (~> @loot-deck list->vector)
                            #:entry->row (compose1 vector ~a)
