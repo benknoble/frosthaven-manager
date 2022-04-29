4fc5d28e01ae9750a48252870df87e45f7545446
D. Ben Knoble <ben.knoble+github@gmail.com>
2022-04-29 08:42:55 -0400

    use callbacks in player-view

--- player-info.rkt
+++ player-info.rkt
@@ -4,10 +4,16 @@
            [player-input-views (-> (obs/c (listof (cons/c natural-number/c
                                                           (obs/c player?))))
                                    (is-a?/c view<%>))]
-           [player-view (-> (obs/c player?) (is-a?/c view<%>))]))
+           [player-view (->* ((obs/c player?))
+                             (#:on-condition (-> (list/c condition? boolean?) any)
+                              #:on-hp (-> (-> number? number?) any)
+                              #:on-xp (-> (-> number? number?) any)
+                              #:on-initiative (-> number? any))
+                             (is-a?/c view<%>))]))
 
 (require racket/gui/easy
          "observable-operator.rkt"
+         "qi.rkt"
          racket/gui/easy/contract
          "defns.rkt")
 
@@ -33,30 +39,29 @@
                             (struct-copy player p [max-hp new-hp]))])))))
              #:min-size (@~> @players (~>> length (* 40) (list #f)))))
 
-(define (player-view @player)
+(define (player-view @player
+                     #:on-condition [on-condition void]
+                     #:on-hp [on-hp void]
+                     #:on-xp [on-xp void]
+                     #:on-initiative [on-initiative void])
   (define (make-condition-checkbox c)
     (checkbox #:label (~a c)
               #:checked? (@> @player (afflicted-by? c))
-              (match-lambda
-                [#f (<@ @player (remove-condition c))]
-                [#t (<@ @player (add-condition c))])))
+              (flow (~>> (list c) on-condition))))
   (define hp-panel
-    (hpanel (button "-" (thunk (if (@! (@> @player dead?))
-                                 (void)
-                                 (<@ @player (act-on-hp sub1)))))
+    (hpanel (button "-" (thunk (unless (@! (@> @player dead?))
+                                 (on-hp sub1))))
             (text (@> @player
                       (match-lambda
                         [(struct* player ([max-hp max] [current-hp current]))
                          (~a "HP: " current "/" max)])))
-            (button "+" (thunk (if (@! (@> @player at-max-health?))
-                                 (void)
-                                 (<@ @player (act-on-hp add1)))))))
+            (button "+" (thunk (unless (@! (@> @player at-max-health?))
+                                 (on-hp add1))))))
   (define xp-panel
-    (hpanel (button "-" (thunk (if (@! (@~> @player (~> player-xp zero?)))
-                                 (void)
-                                 (<@ @player (act-on-xp sub1)))))
+    (hpanel (button "-" (thunk (unless (@! (@~> @player (~> player-xp zero?)))
+                                 (on-xp sub1))))
             (text (@~> @player (~>> player-xp (~a "XP: "))))
-            (button "+" (thunk (<@ @player (act-on-xp add1))))))
+            (button "+" (thunk (on-xp add1)))))
   (define name-hp-xp
     (vpanel #:alignment '(center center)
             #:stretch '(#f #t)
@@ -73,12 +78,12 @@
                 (thunk
                   (render
                     (dialog
-                      (slider @init
-                              (λ (i)
-                                (<~@ @player (set-initiative i)))
-                              #:min-value 0
-                              #:max-value 99
-                              #:label (@~> @player (~>> player-name (~a "Initiative for ")))))))))))
+                      (slider
+                        @init
+                        on-initiative
+                        #:min-value 0
+                        #:max-value 99
+                        #:label (@~> @player (~>> player-name (~a "Initiative for ")))))))))))
   (define conditions-panel
     (vpanel (text (@~> @player (~> player-conditions
                                    (sep ~a) collect
@@ -105,6 +110,17 @@
     (button "+" (thunk (on-hp add1)))))
 
 (module+ main
+  (define (condition-handler @player)
+    (match-lambda
+      [`(,c #f) (<@ @player (remove-condition c))]
+      [`(,c #t) (<@ @player (add-condition c))]))
+  (define ((hp-handler @player) proc)
+    (<@ @player (act-on-hp proc)))
+  (define ((xp-handler @player) proc)
+    (<@ @player (act-on-xp proc)))
+  (define ((initiative-handler @player) i)
+    (<~@ @player (set-initiative i)))
+
   (define/obs @players
     (list
       (cons 0 (@ (player "A" 15 10 3 (list regenerate invisible immobilize) 23)))
@@ -115,8 +131,14 @@
     (render (window i-view))
     (render
       (window
-        (vpanel (player-view (cdr (first (@! @players))))
-                (spacer)
-                (player-view (cdr (second (@! @players))))
-                (spacer)
-                (player-view (cdr (third (@! @players)))))))))
+        (apply
+          vpanel
+          (map (match-lambda
+                 [(cons _ @p)
+                  (player-view
+                    @p
+                    #:on-condition (condition-handler @p)
+                    #:on-hp (hp-handler @p)
+                    #:on-xp (xp-handler @p)
+                    #:on-initiative (initiative-handler @p))])
+               (@! @players)))))))
