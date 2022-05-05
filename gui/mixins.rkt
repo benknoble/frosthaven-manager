11b7996c00cac7a172930bfbe19522cca516f933
D. Ben Knoble <ben.knoble+github@gmail.com>
2022-05-05 16:54:17 -0400

    add useful mixins

---
+++ gui/mixins.rkt
@@ -0,0 +1,29 @@
+#lang racket
+
+;; cf. https://github.com/Bogdanp/racket-gui-easy/issues/13#issuecomment-1093700596
+;; cf. https://github.com/Bogdanp/racket-gui-easy/issues/13#issuecomment-1093153178
+
+(provide make-closing-proc-mixin
+         make-on-close-mixin)
+
+;; Dialogs need to be closed, but rendering a dialog yields so there's
+;; no way to retrieve a dialog's renderer from within itself.  This
+;; may be another argument for gui-easy providing a managed
+;; `current-renderer'.  In the mean time, we can abuse mixins for this
+;; purpose.
+
+;; calls `out` with `close-proc`, which closes the window when invoked
+(define ((make-closing-proc-mixin out) %)
+  (class %
+    (super-new)
+    (out (λ ()
+           (when (send this can-close?)
+             (send this on-close)
+             (send this show #f))))))
+
+;; calls `proc` when the window closes
+(define ((make-on-close-mixin proc) %)
+  (class %
+    (super-new)
+    (define/augment (on-close)
+      (proc))))
