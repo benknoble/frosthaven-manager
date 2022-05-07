6d45abc5e70932e056d75cc75ccf6fa3473d14cf
D. Ben Knoble <ben.knoble+github@gmail.com>
2022-05-07 11:39:06 -0400

    gui: convert lambda to defines

--- gui.rkt
+++ gui.rkt
@@ -22,14 +22,79 @@
   (define/obs @players empty)
   (define/obs @loot-deck empty)
   (define/obs @num-loot-cards 0)
+  (define-values (@elements elements-view) (elements-cycler elements))
+  ;; functions
+  (define (make-player-entry i)
+    (cons i (make-player "" 1)))
   (define (update-players players k f)
-    (map (λ (e)
-           (if (eq? (car e) k)
-             (cons k (f (cdr e)))
-             e))
-         players))
+    (define (maybe-update-player e)
+      (if (eq? (car e) k)
+        (cons k (f (cdr e)))
+        e))
+    (map maybe-update-player players))
   (define (update-all-players players f)
-    (map (match-lambda [(cons id p) (cons id (f p))]) players))
+    (define update-player
+      (match-lambda [(cons id p) (cons id (f p))]))
+    (map update-player players))
+  (define (set-level level)
+    (:= @level level))
+  (define (set-num-players num-players)
+    (:= @num-players num-players))
+  (define (to-input-player-info)
+    (when (empty? (@! @players))
+      (:= @players (build-list (@! @num-players) make-player-entry)))
+    (:= @mode 'input-player-info))
+  (define (update-player-name k name)
+    (<~@ @players (update-players k (update-name name))))
+  (define (update-player-max-hp k f)
+    (<~@ @players (update-players k (act-on-max-hp f))))
+  (define (to-build-loot-deck)
+    ;; give each player max-hp
+    (<~@ @players
+         (update-all-players
+           (flow (~> (-< (~> player-max-hp const act-on-hp)
+                         _)
+                     apply))))
+    (:= @mode 'build-loot-deck))
+  (define (to-play)
+    (:= @mode 'play))
+  (define-flow (update-deck-and-num-loot-cards loot-event)
+    (-< (loot-picker-updater @loot-deck)
+        ;; order important
+        (gen (:= @num-loot-cards (length (@! @loot-deck))))))
+  (define (make-player-view k @e)
+    (define (update proc)
+      (<~@ @players (update-players k proc)))
+    (define-flow update-condition (~> condition-handler update))
+    (define-flow update-hp (~> act-on-hp update))
+    (define-flow update-xp (~> act-on-xp update))
+    (define (update-initiative i) (update (flow (set-initiative i))))
+    (player-view
+      (@> @e cdr)
+      @num-players
+      #:on-condition update-condition
+      #:on-hp update-hp
+      #:on-xp update-xp
+      #:on-initiative update-initiative))
+  (define (next-round)
+    ;; wane elements
+    (for-each (flow (<@ wane-element)) @elements)
+    ;; reset player initiative
+    (<~@ @players (update-all-players clear-initiative)))
+  (define (take-loot)
+    (<@ @loot-deck rest))
+  (define (give-player-loot* p)
+    (define card
+      (@! (@~> @loot-deck (if (not empty?) first #f))))
+    (if card
+      ((add-loot card) p)
+      p))
+  (define (give-player-loot k)
+    (<~@ @players (update-players k give-player-loot*)))
+  (define (draw)
+    ;; order players
+    (<~@ @players
+         (sort < #:key (flow (~> cdr player-initiative)))))
   ;; gui
   (render
     (window
@@ -37,95 +102,47 @@
       #:size '(700 300)
       (case-view @mode
         [(start)
-         (vpanel (start-view #:on-level (λ:= @level identity)
-                             #:on-player (λ:= @num-players identity))
-                 (button "Play"
-                         (thunk (when (empty? (@! @players))
-                                  (:= @players (build-list
-                                                 (@! @num-players)
-                                                 (λ (i) (cons i (make-player "" 1))))))
-                                (:= @mode 'input-player-info))))]
+         (vpanel (start-view #:on-level set-level
+                             #:on-player set-num-players)
+                 (button "Play" to-input-player-info))]
         [(input-player-info)
-         (vpanel
-           (player-input-views
-             @num-players
-             #:on-name (λ (k name)
-                         (<~@ @players (update-players k (update-name name))))
-             #:on-hp (λ (k f)
-                       (<~@ @players (update-players k (act-on-max-hp f)))))
-           (button "Next"
-                   (thunk
-                     ;; give each player max-hp
-                     (<~@ @players
-                          (update-all-players
-                            (flow (~> (-< (~> player-max-hp const act-on-hp)
-                                          _)
-                                      apply))))
-                     (:= @mode 'build-loot-deck))))]
+         (vpanel (player-input-views @num-players
+                                     #:on-name update-player-name
+                                     #:on-hp update-player-max-hp)
+                 (button "Next" to-build-loot-deck))]
         [(build-loot-deck)
-         (vpanel (loot-picker #:on-card (flow (-< (loot-picker-updater @loot-deck)
-                                                  ;; order important
-                                                  (gen (:= @num-loot-cards (length (@! @loot-deck)))))))
+         (vpanel (loot-picker #:on-card update-deck-and-num-loot-cards)
                  (spacer)
-                 (button "Next"
-                         (thunk (:= @mode 'play))))]
+                 (button "Next" to-play))]
         [(play)
-         (let-values ([(@elements elements-view) (elements-cycler elements)])
-           (vpanel
-             (hpanel
-               #:stretch '(#t #f)
-               (spacer)
-               elements-view)
-             (spacer)
-             (list-view @players
-               #:min-size (@~> @players (~>> length (* 100) (list #f)))
-               #:key car
-               (λ (k @e)
-                 (define (update proc)
-                   (<~@ @players (update-players k proc)))
-                 (player-view
-                   (@> @e cdr)
-                   @num-players
-                   #:on-condition (flow (~> condition-handler update))
-                   #:on-hp (flow (~> act-on-hp update))
-                   #:on-xp (flow (~> act-on-xp update))
-                   #:on-initiative (λ (i) (update (flow (set-initiative i)))))))
-             (spacer)
-             (hpanel
-               #:stretch '(#t #f)
-               (button "Next Round"
-                       (thunk
-                         ;; wane elements
-                         (for-each (flow (<@ wane-element)) @elements)
-                         ;; reset player initiative
-                         (<~@ @players (update-all-players clear-initiative))))
-               (spacer)
-               (loot-button
-                 @loot-deck @num-loot-cards @num-players @players
-                 ;; valid because only enabled if loot-deck non-empty, and only
-                 ;; closing if loot assigned
-                 #:on-close (thunk (<@ @loot-deck rest))
-                 #:on-player
-                 (λ (k)
-                   (<~@ @players
-                        (update-players
-                          k
-                          (λ (p)
-                            (define card
-                              (@! (@~> @loot-deck (if (not empty?) first #f))))
-                            (if card
-                              ((add-loot card) p)
-                              p))))))
-               (spacer)
-               (button "Draw!"
-                       (thunk
-                         ;; order players
-                         (<~@ @players
-                              (sort < #:key (flow (~> cdr player-initiative)))))))
-             (hpanel
-               #:stretch '(#f #f)
-               (level-stats @level @num-players)
-               (vpanel
-                 (level-table @level)
-                 (inspiration-table @num-players)))))]
+         (vpanel
+           ;; top
+           (hpanel #:stretch '(#t #f)
+                   (spacer)
+                   elements-view)
+           (spacer)
+           ;; main
+           (list-view @players
+             #:min-size (@~> @players (~>> length (* 100) (list #f)))
+             #:key car
+             make-player-view)
+           (spacer)
+           ;; bottom (1)
+           (hpanel #:stretch '(#t #f)
+                   (button "Next Round" next-round)
+                   (spacer)
+                   (loot-button
+                     @loot-deck @num-loot-cards @num-players @players
+                     ;; valid because only enabled if loot-deck non-empty, and only
+                     ;; closing if loot assigned
+                     #:on-close take-loot
+                     #:on-player give-player-loot)
+                   (spacer)
+                   (button "Draw!" draw))
+           ;; bottom (2)
+           (hpanel #:stretch '(#f #f)
+                   (level-stats @level @num-players)
+                   (vpanel
+                     (level-table @level)
+                     (inspiration-table @num-players))))]
         [else (text "Broken")]))))
