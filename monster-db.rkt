487582366c64d674aeb2cf37e3391a606db3b876
D. Ben Knoble <ben.knoble+github@gmail.com>
2022-05-08 16:30:14 -0400

    unquote test data

--- monster-db.rkt
+++ monster-db.rkt
@@ -133,96 +133,96 @@
   (define-values (info actions)
     (get-dbs "sample-db.rktd"))
   (check-equal? info
-                '#hash(("archer"
-                        .
-                        #hash(("hynox archer"
-                               .
-                               #s(monster-info
-                                   "archer"
-                                   "hynox archer"
-                                   (#s(monster-stats 2 2 2 () () ())
-                                    #s(monster-stats 3 3 3 () () ())
-                                    #s(monster-stats 4 4 4 () () ())
-                                    #s(monster-stats 5 5 5 () () ())
-                                    #s(monster-stats 6 6 6 () () ())
-                                    #s(monster-stats 7 7 7 () () ())
-                                    #s(monster-stats 8 8 8 () () ())
-                                    #s(monster-stats 9 9 9 () () ()))
-                                   (#s(monster-stats 4 2 3 ("shield 1") () ())
-                                    #s(monster-stats 5 3 4 ("shield 1") () ())
-                                    #s(monster-stats 6 4 5 ("shield 1") () ())
-                                    #s(monster-stats 7 5 6 ("shield 2") () ())
-                                    #s(monster-stats 8 6 7 ("shield 2") () ())
-                                    #s(monster-stats 9 7 8 ("shield 2") () ())
-                                    #s(monster-stats 10 8 9 ("shield 3") () ())
-                                    #s(monster-stats 11 9 10 ("shield 3") () ()))))
-                              ("wyrmling archer"
-                               .
-                               #s(monster-info
-                                   "archer"
-                                   "wyrmling archer"
-                                   (#s(monster-stats 1 1 1 () () ())
-                                    #s(monster-stats 2 2 2 () () ())
-                                    #s(monster-stats 3 3 3 () () ())
-                                    #s(monster-stats 4 4 4 () () ())
-                                    #s(monster-stats 5 5 5 () () ())
-                                    #s(monster-stats 6 6 6 () () ())
-                                    #s(monster-stats 7 7 7 () () ())
-                                    #s(monster-stats 8 8 8 () () ()))
-                                   (#s(monster-stats 3 1 2 ("shield 1") () ())
-                                    #s(monster-stats 4 2 3 ("shield 1") () ())
-                                    #s(monster-stats 5 3 4 ("shield 1") () ())
-                                    #s(monster-stats 6 4 5 ("shield 2") () ())
-                                    #s(monster-stats 7 5 6 ("shield 2") () ())
-                                    #s(monster-stats 8 6 7 ("shield 2") () ())
-                                    #s(monster-stats 9 7 8 ("shield 3") () ())
-                                    #s(monster-stats 10 8 9 ("shield 3") () ()))))))
-                       ("guard"
-                        .
-                        #hash(("hynox guard"
-                               .
-                               #s(monster-info
-                                   "guard"
-                                   "hynox guard"
-                                   (#s(monster-stats 2 2 2 () () ())
-                                    #s(monster-stats 3 3 3 () () ())
-                                    #s(monster-stats 4 4 4 () () ())
-                                    #s(monster-stats 5 5 5 () () ())
-                                    #s(monster-stats 6 6 6 () () ())
-                                    #s(monster-stats 7 7 7 () () ())
-                                    #s(monster-stats 8 8 8 () () ())
-                                    #s(monster-stats 9 9 9 () () ()))
-                                   (#s(monster-stats 4 2 3 ("shield 1") () ())
-                                    #s(monster-stats 5 3 4 ("shield 1") () ())
-                                    #s(monster-stats 6 4 5 ("shield 1") () ())
-                                    #s(monster-stats 7 5 6 ("shield 2") () ())
-                                    #s(monster-stats 8 6 7 ("shield 2") () ())
-                                    #s(monster-stats 9 7 8 ("shield 2") () ())
-                                    #s(monster-stats 10 8 9 ("shield 3") () ())
-                                    #s(monster-stats 11 9 10 ("shield 3") () ()))))))))
+                #hash(("archer"
+                       .
+                       #hash(("hynox archer"
+                              .
+                              #s(monster-info
+                                  "archer"
+                                  "hynox archer"
+                                  (#s(monster-stats 2 2 2 () () ())
+                                   #s(monster-stats 3 3 3 () () ())
+                                   #s(monster-stats 4 4 4 () () ())
+                                   #s(monster-stats 5 5 5 () () ())
+                                   #s(monster-stats 6 6 6 () () ())
+                                   #s(monster-stats 7 7 7 () () ())
+                                   #s(monster-stats 8 8 8 () () ())
+                                   #s(monster-stats 9 9 9 () () ()))
+                                  (#s(monster-stats 4 2 3 ("shield 1") () ())
+                                   #s(monster-stats 5 3 4 ("shield 1") () ())
+                                   #s(monster-stats 6 4 5 ("shield 1") () ())
+                                   #s(monster-stats 7 5 6 ("shield 2") () ())
+                                   #s(monster-stats 8 6 7 ("shield 2") () ())
+                                   #s(monster-stats 9 7 8 ("shield 2") () ())
+                                   #s(monster-stats 10 8 9 ("shield 3") () ())
+                                   #s(monster-stats 11 9 10 ("shield 3") () ()))))
+                             ("wyrmling archer"
+                              .
+                              #s(monster-info
+                                  "archer"
+                                  "wyrmling archer"
+                                  (#s(monster-stats 1 1 1 () () ())
+                                   #s(monster-stats 2 2 2 () () ())
+                                   #s(monster-stats 3 3 3 () () ())
+                                   #s(monster-stats 4 4 4 () () ())
+                                   #s(monster-stats 5 5 5 () () ())
+                                   #s(monster-stats 6 6 6 () () ())
+                                   #s(monster-stats 7 7 7 () () ())
+                                   #s(monster-stats 8 8 8 () () ()))
+                                  (#s(monster-stats 3 1 2 ("shield 1") () ())
+                                   #s(monster-stats 4 2 3 ("shield 1") () ())
+                                   #s(monster-stats 5 3 4 ("shield 1") () ())
+                                   #s(monster-stats 6 4 5 ("shield 2") () ())
+                                   #s(monster-stats 7 5 6 ("shield 2") () ())
+                                   #s(monster-stats 8 6 7 ("shield 2") () ())
+                                   #s(monster-stats 9 7 8 ("shield 3") () ())
+                                   #s(monster-stats 10 8 9 ("shield 3") () ()))))))
+                      ("guard"
+                       .
+                       #hash(("hynox guard"
+                              .
+                              #s(monster-info
+                                  "guard"
+                                  "hynox guard"
+                                  (#s(monster-stats 2 2 2 () () ())
+                                   #s(monster-stats 3 3 3 () () ())
+                                   #s(monster-stats 4 4 4 () () ())
+                                   #s(monster-stats 5 5 5 () () ())
+                                   #s(monster-stats 6 6 6 () () ())
+                                   #s(monster-stats 7 7 7 () () ())
+                                   #s(monster-stats 8 8 8 () () ())
+                                   #s(monster-stats 9 9 9 () () ()))
+                                  (#s(monster-stats 4 2 3 ("shield 1") () ())
+                                   #s(monster-stats 5 3 4 ("shield 1") () ())
+                                   #s(monster-stats 6 4 5 ("shield 1") () ())
+                                   #s(monster-stats 7 5 6 ("shield 2") () ())
+                                   #s(monster-stats 8 6 7 ("shield 2") () ())
+                                   #s(monster-stats 9 7 8 ("shield 2") () ())
+                                   #s(monster-stats 10 8 9 ("shield 3") () ())
+                                   #s(monster-stats 11 9 10 ("shield 3") () ()))))))))
   (check-equal? actions
-                '#hash(("archer"
-                        .
-                        (#s(monster-action
-                             "archer"
-                             "double-shot"
-                             25
-                             ("move +1"
-                              "attack +2, range 5"
-                              "attack +2, range 5, +1 if same target")
-                             #f)
-                         #s(monster-action
-                             "archer"
-                             "take aim"
-                             80
-                             ("move +2" "strengthen self")
-                             #t)))
-                       ("guard"
-                        .
-                        (#s(monster-action
-                             "guard"
-                             "rushing charge"
-                             25
-                             ("move +3" "attack +2 + number of spaces moved towards target")
-                             #f)
-                         #s(monster-action "guard" "stand tall" 80 ("shield 3") #t))))))
+                #hash(("archer"
+                       .
+                       (#s(monster-action
+                            "archer"
+                            "double-shot"
+                            25
+                            ("move +1"
+                             "attack +2, range 5"
+                             "attack +2, range 5, +1 if same target")
+                            #f)
+                        #s(monster-action
+                            "archer"
+                            "take aim"
+                            80
+                            ("move +2" "strengthen self")
+                            #t)))
+                      ("guard"
+                       .
+                       (#s(monster-action
+                            "guard"
+                            "rushing charge"
+                            25
+                            ("move +3" "attack +2 + number of spaces moved towards target")
+                            #f)
+                        #s(monster-action "guard" "stand tall" 80 ("shield 3") #t))))))
