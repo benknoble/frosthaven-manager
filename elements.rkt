7b6b28e407af7642c7b3fded2d32b4fd1bbdbfe3
D. Ben Knoble <ben.knoble+github@gmail.com>
2022-05-11 15:55:41 -0400

    give elements names

--- elements.rkt
+++ elements.rkt
@@ -2,7 +2,8 @@
 
 (provide
   (contract-out
-    [struct element-pics ([infused pict?]
+    [struct element-pics ([name string?]
+                          [infused pict?]
                           [waning pict?]
                           [unfused pict?])]
     [fire element-pics?]
@@ -22,7 +23,7 @@
                   triangle
                   wedge))
 
-(struct element-pics [infused waning unfused] #:transparent)
+(struct element-pics [name infused waning unfused] #:transparent)
 
 (define size 50)
 
@@ -36,7 +37,7 @@
 (define infused-fire (cc-superimpose (red base) fire-overlay))
 (define waning-fire (cc-superimpose (wane "red") fire-overlay))
 (define unfused-fire (cc-superimpose base fire-overlay))
-(define fire (element-pics infused-fire waning-fire unfused-fire))
+(define fire (element-pics "Fire" infused-fire waning-fire unfused-fire))
 
 (define ice-overlay
   (let* ([bar (inset (white (filled-rectangle 2 (- size 5))) 5 0)]
@@ -57,26 +58,26 @@
 (define infused-ice (cc-superimpose (cyan base) ice-overlay))
 (define waning-ice (cc-superimpose (wane "cyan") ice-overlay))
 (define unfused-ice (cc-superimpose base ice-overlay))
-(define ice (element-pics infused-ice waning-ice unfused-ice))
+(define ice (element-pics "Ice" infused-ice waning-ice unfused-ice))
 
 (define air-overlay (scale-to-fit (text "💨") base))
 (define infused-air (cc-superimpose (colorize base "light gray") air-overlay))
 (define waning-air (cc-superimpose (wane "light gray") air-overlay))
 (define unfused-air (cc-superimpose base air-overlay))
-(define air (element-pics infused-air waning-air unfused-air))
+(define air (element-pics "Air" infused-air waning-air unfused-air))
 
 (define earth-overlay (scale-to-fit (text "🌿") base))
 (define infused-earth (cc-superimpose (colorize base "dark green") earth-overlay))
 (define waning-earth (cc-superimpose (wane "dark green") earth-overlay))
 (define unfused-earth (cc-superimpose base earth-overlay))
-(define earth (element-pics infused-earth waning-earth unfused-earth))
+(define earth (element-pics "Earth" infused-earth waning-earth unfused-earth))
 
 (define light-overlay (cc-superimpose (white (outline-flash (- size 5) (- size 5) 8 .55))
                                       (white (filled-flash (- size 25) (- size 25) 8 .55))))
 (define infused-light (cc-superimpose (colorize base "gold") light-overlay))
 (define waning-light (cc-superimpose (wane "gold") light-overlay))
 (define unfused-light (cc-superimpose base light-overlay))
-(define light (element-pics infused-light waning-light unfused-light))
+(define light (element-pics "Light" infused-light waning-light unfused-light))
 
 (define infused-dark (pin-over (pin-over (colorize base "purple")
                                          (- (/ size 2) 6) (/ size 4)
@@ -93,7 +94,7 @@
                                          (disk (/ size 2) #:color "white" #:border-color "black" #:border-width 1))
                                6 (/ size 4)
                                (disk (/ size 2) #:color "black" #:border-color "white" #:border-width 1)))
-(define dark (element-pics infused-dark waning-dark unfused-dark))
+(define dark (element-pics "Dark" infused-dark waning-dark unfused-dark))
 
 (define elements (list fire ice air earth light dark))
 
