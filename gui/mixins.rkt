8f82798779a6fe97037709698a06f7a383e4c098
D. Ben Knoble <ben.knoble+github@gmail.com>
2022-07-28 10:26:05 -0400

    use mixin form

--- gui/mixins.rkt
+++ gui/mixins.rkt
@@ -6,6 +6,8 @@
 (provide make-closing-proc-mixin
          make-on-close-mixin)
 
+(require (only-in racket/gui/base top-level-window<%>))
+
 ;; Dialogs need to be closed, but rendering a dialog yields so there's
 ;; no way to retrieve a dialog's renderer from within itself.  This
 ;; may be another argument for gui-easy providing a managed
@@ -13,17 +15,15 @@
 ;; purpose.
 
 ;; calls `out` with `close-proc`, which closes the window when invoked
-(define ((make-closing-proc-mixin out) %)
-  (class %
-    (super-new)
+(define (make-closing-proc-mixin out)
+  (mixin (top-level-window<%>) (top-level-window<%>) (super-new)
     (out (λ ()
            (when (send this can-close?)
              (send this on-close)
              (send this show #f))))))
 
 ;; calls `proc` when the window closes
-(define ((make-on-close-mixin proc) %)
-  (class %
-    (super-new)
+(define (make-on-close-mixin proc)
+  (mixin (top-level-window<%>) (top-level-window<%>) (super-new)
     (define/augment (on-close)
       (proc))))
