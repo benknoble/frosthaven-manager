2b9815b48bb227dab05a52828c53ef10c41fd28a
D. Ben Knoble <ben.knoble+github@gmail.com>
2022-10-28 16:42:29 -0400

    draw my own fire overlay
    Close #2

--- elements.rkt
+++ elements.rkt
@@ -30,6 +30,7 @@
 (define trimmed-size (- size 5))
 
 (define-flow half (* 1/2))
+(define-flow avg (~> (-< + count) /))
 
 (define base (disk size))
 
@@ -42,10 +43,71 @@
           (~> (half-wedge color) (rotate pi)))
       vc-append))
 
-(define fire-overlay (scale-to-fit (text "🔥") base))
-(define infused-fire (cc-superimpose (red base) fire-overlay))
-(define waning-fire (cc-superimpose (wane "red") fire-overlay))
-(define unfused-fire (cc-superimpose base fire-overlay))
+(define (fire-shape path border-color fill-color)
+  (translate
+   (dc (lambda (dc dx dy)
+        (define old-brush (send dc get-brush))
+        (define old-pen (send dc get-pen))
+        (send* dc
+          (set-brush (new brush% [color fill-color]))
+          (set-pen (new pen% [color border-color]))
+          (draw-path path dx dy)
+          (set-brush old-brush)
+          (set-pen old-pen)))
+      50 50)
+   -5 0))
+
+(define (fire-overlay color1 color2 color3 color4)
+  (let* ([q-points (list
+                     (list (list 25 45) (list 18 42))
+                     (list (list 15 30) (list 16 23))
+                     (list (list 18 19) (list 22 12))
+                     (list (list 20 07) (list 29 11))
+                     (list (list 35 27) (list 37 28))
+                     (list (list 40 25) (list 41 21))
+                     (list (list 39 18) (list 46 27))
+                     (list (list 43 35) (list 39 43))
+                     ;; (first _) must be same as (first (first q-points))
+                     (list (list 25 45) #f))]
+         [first-point (first (first q-points))]
+         [better-path
+           (let ([p (new dc-path%)])
+             (match-define (list x0 y0) first-point)
+             (begin0
+               p
+               (send p move-to x0 y0)
+               (for ([pc1 (in-list q-points)]
+                     [pc2 (in-list (cdr q-points))])
+                 (match-define (list (list p1x p1y) (list cx cy)) pc1)
+                 (match-define (list (list p2x p2y) _) pc2)
+                 ;;
+                 (define x21 (avg p1x cx))
+                 (define y21 (avg p1y cy))
+                 (send p line-to x21 y21)
+                 ;;
+                 (define x22 (avg cx p2x))
+                 (define y22 (avg cy p2y))
+                 (define xm1 (avg x21 cx))
+                 (define ym1 (avg y21 cy))
+                 (define xm2 (avg cx x22))
+                 (define ym2 (avg cy y22))
+                 (send p curve-to xm1 ym1 xm2 ym2 x22 y22)
+                 ;;
+                 (send p line-to p2x p2y))))]
+         [fire-shape (flow (fire-shape better-path "black" _))])
+    (~> (color1 color2 color3 color4)
+        (>< fire-shape)
+        (== _
+            (~> (scale 0.7 0.7) (translate 0 5))
+            (~> (scale 0.5 0.5) (translate 0 9))
+            (~> (scale 0.3 0.2) (translate 0 14)))
+        cc-superimpose)))
+
+(define colored-fire-overlay (fire-overlay "red" "orange" "yellow" "white"))
+(define bw-fire-overlay (fire-overlay "white" "black" "white" "black"))
+(define infused-fire (cc-superimpose (red base) colored-fire-overlay))
+(define waning-fire (cc-superimpose (wane "red") colored-fire-overlay))
+(define unfused-fire (cc-superimpose base bw-fire-overlay))
 (define fire (element-pics "Fire" infused-fire waning-fire unfused-fire))
 
 (define ice-overlay
