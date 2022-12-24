97d7577f98a78099787064f84117aed389baa332
D. Ben Knoble <ben.knoble+github@gmail.com>
2022-12-23 21:05:47 -0500

    create serializable-enum helpers

--- enum-helpers.rkt
+++ enum-helpers.rkt
@@ -1,12 +1,22 @@
 #lang racket
 
 (provide
+  define-serializable-enum-type
   (contract-out
     [make-property-maker-that-displays-as-constant-names
       (-> uninitialized-enum-descriptor?
-          (listof (cons/c struct-type-property? any/c)))]))
+          (listof (cons/c struct-type-property? any/c)))]
+    [compose-property-makers
+      (-> (-> uninitialized-enum-descriptor?
+              (listof (cons/c struct-type-property? any/c))) ...
+          (-> uninitialized-enum-descriptor?
+              (listof (cons/c struct-type-property? any/c))))]))
 
 (require
+  racket/serialize
+  (for-syntax racket/syntax)
+  syntax/parse
+  syntax/parse/define
   rebellion/type/enum
   rebellion/collection/keyset
   frosthaven-manager/qi)
@@ -31,3 +41,47 @@
                 ;; everything else
                 [(v out mode) (custom-writer v out mode)]))
         default-props-sans-custom-writer))
+
+(define-syntax-parse-rule
+  (define-serializable-enum-type
+    type:id
+    (constant:id ...)
+    {~alt
+      {~optional {~and #:omit-root-binding omit-root-binding-kw}}
+      {~optional {~seq #:descriptor-name descriptor:id}}
+      {~optional {~seq #:predicate-name predicate:id}}
+      {~optional {~seq #:discriminator-name discriminator:id}}
+      {~optional {~seq #:selector-name selector:id}}
+      {~optional {~seq #:inspector inspector:expr}}
+      {~optional {~seq #:property-maker prop-maker:expr}}} ...)
+  #:with deserialize-info (format-id #'type "deserialize-info:~a" #'type #:source #'type)
+  #:with default-selector (format-id #'type "selector:~a" #'type)
+  (begin
+    (define-enum-type type
+      (constant ...)
+      {~? omit-root-binding-kw}
+      {~? {~@ #:descriptor-name descriptor}}
+      {~? {~@ #:predicate-name predicate}}
+      {~? {~@ #:discriminator-name discriminator}}
+      {~? {~@ #:selector-name selector}}
+      {~? {~@ #:inspector inspector}}
+      #:property-maker (compose-property-makers
+                         {~? prop-maker default-enum-properties}
+                         (serializable-property-maker #'deserialize-info)))
+    (provide deserialize-info)
+    (define deserialize-info
+      (make-deserialize-info
+        {~? selector default-selector}
+        (thunk (error 'type "cycles not supported"))))))
+
+(define ((serializable-property-maker deserialize-info-binding) desc)
+  (define discrim (enum-descriptor-discriminator desc))
+  (list (cons prop:serializable
+              (make-serialize-info
+                (λ (x) (vector (discrim x)))
+                deserialize-info-binding
+                #f
+                (or (current-load-relative-directory) (current-directory))))))
+
+(define ((compose-property-makers . ps) desc)
+  (append-map (λ (p) (p desc)) ps))
--- scribblings/reference.scrbl
+++ scribblings/reference.scrbl
@@ -571,6 +571,28 @@ that the @racket[display] string is the same as the constant name as in
 @racket[define-enum-type].
 }
 
+@defproc[(compose-property-makers [p (-> uninitialized-enum-descriptor? (listof (cons/c struct-type-property? any/c)))] ...)
+         (-> uninitialized-enum-descriptor? (listof (cons/c struct-type-property? any/c)))]{
+Creates a property maker suitable for @racket[define-enum-type] that combines
+each @racket[p].
+}
+
+@defform[(define-serializable-enum-type id (constant-id ...) enum-option ...)
+         #:grammar ([enum-option #:omit-root-binding
+                                 (code:line #:descriptor-name descriptor-id)
+                                 (code:line #:predicate-name predicate-id)
+                                 (code:line #:discriminator-name discriminator-id)
+                                 (code:line #:selector-name selector-id)
+                                 (code:line #:property-maker prop-maker-expr)
+                                 (code:line #:inspector inspector-expr)])
+         #:contracts ([prop-maker-expr (-> uninitialized-enum-descriptor?
+                                           (listof (cons/c struct-type-property? any/c)))]
+                      [inspector-expr inspector?])]{
+Exactly like @racket[define-enum-type], but with the addition of
+@racket[prop:serializable] via a deserialize-info named
+@racketidfont{deserialize-info:}@racket[id].
+}
+
 @section{@tt{manager}}
 @defmodule[frosthaven-manager/manager]
 
