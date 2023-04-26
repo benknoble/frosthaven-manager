f9fbc3ae48bbc6cd9dca4f80dcb4c4f1f6eedb80
D. Ben Knoble <ben.knoble+github@gmail.com>
2023-04-26 12:14:14 -0400

    meta: specify UTF-8

--- server.rkt
+++ server.rkt
@@ -317,7 +317,8 @@
             " may be the one you wanted.")))))
 
 (define common-heads
-  `((meta ([name "viewport"] [content "width=device-width, initial-scale=1.0"]))))
+  `((meta ([name "viewport"] [content "width=device-width, initial-scale=1.0"]))
+    (meta ([charset "UTF-8"]))))
 
 (define (url-sans-param u)
   (struct-copy url u [path (map path/param-sans-param (url-path u))]))
