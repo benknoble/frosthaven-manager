ae343108325955ed386fa860959afb05de2c88c5
D. Ben Knoble <ben.knoble+github@gmail.com>
2023-11-07 13:47:29 -0500

    test gui/formula-editor

--- gui/formula-editor.rkt
+++ gui/formula-editor.rkt
@@ -12,27 +12,13 @@
          frosthaven-manager/parsers/formula
          frosthaven-manager/gui/render)
 
+(module+ test (require rackunit))
+
 (define (formula-editor @env #:mix [mix values])
   (define/obs @input "")
-  (define @result
-    (obs-combine
-      (λ (inp env)
-        (with-handlers ([exn:fail:read:megaparsack?
-                          (λ (exn)
-                            (parse-error->string
-                              (message (match (exn:fail:read-srclocs exn)
-                                         [(cons (srcloc _ line column posn span) _)
-                                          (srcloc "Formula" line column posn span)]
-                                         [_ (make-srcloc "Formula" #f #f #f #f)])
-                                       (exn:fail:read:megaparsack-unexpected exn)
-                                       (exn:fail:read:megaparsack-expected exn))))]
-                        [exn:fail:contract? (const "reference to unavailable variable")])
-          ((parse-expr inp) env)))
-      @input @env))
+  (define @result (obs-combine eval-formula @input @env))
   (define (@var-display var)
-    (@~> @env (~> (hash-ref var #f)
-                  (or _ "unavailable")
-                  (~a var ": " _))))
+    (@~> @env (var-label-text var)))
   (window
     #:title "Formula editor"
     #:mixin mix
@@ -63,6 +49,37 @@
                       (render/eventspace
                         #:eventspace closing-eventspace
                         (formula-editor @env #:mix close-custodian-mixin))))))
+
+(define (eval-formula f env)
+  (with-handlers ([exn:fail:read:megaparsack?
+                   (λ (exn)
+                     (parse-error->string
+                      (message (match (exn:fail:read-srclocs exn)
+                                 [(cons (srcloc _ line column posn span) _)
+                                  (srcloc "Formula" line column posn span)]
+                                 [_ (make-srcloc "Formula" #f #f #f #f)])
+                               (exn:fail:read:megaparsack-unexpected exn)
+                               (exn:fail:read:megaparsack-expected exn))))]
+                  [exn:fail:contract? (const "reference to unavailable variable")])
+    ((parse-expr f) env)))
+
+(module+ test
+  (test-case "eval-formula"
+    (check-equal? (eval-formula "3+5" (hash)) 8)
+    (check-equal? (eval-formula "2*L" (hash "L" 7)) 14)
+    (check-regexp-match #rx"variable" (eval-formula "2*L" (hash 'C 7)))
+    (check-regexp-match #rx"Formula:.*: parse error" (eval-formula "/3" (hash)))))
+
+(define (var-label-text env var)
+  (~> (env)
+      (hash-ref var #f)
+      (or _ "unavailable")
+      (~a var ": " _)))
+
+(module+ test
+  (test-case "var-label-text"
+    (check-equal? (var-label-text (hash "C" 4) "C") "C: 4")
+    (check-equal? (var-label-text (hash) "C") "C: unavailable")))
 
 (module+ main
   (define/obs @c 4)
