4405b0f576656538632148a988a639930ba28b82
D. Ben Knoble <ben.knoble+github@gmail.com>
2023-11-07 13:49:41 -0500

    test escape-text

--- gui/helpers.rkt
+++ gui/helpers.rkt
@@ -6,9 +6,17 @@
 
 (require racket/class)
 
+(module+ test (require rackunit))
+
 (define (translate-to-top-coords this top x y)
   (define-values (xs ys) (send this client->screen x y))
   (send top screen->client xs ys))
 
 (define (escape-text s)
   (regexp-replace* #px"&(?!&)" s "&&"))
+
+(module+ test
+  (test-case "escape-text"
+    (check-equal? (escape-text "foo") "foo")
+    (check-equal? (escape-text "Flourish & Fletch & Foo") "Flourish && Fletch && Foo")
+    (check-equal? (escape-text "Flourish && Fletch") "Flourish &&& Fletch")))
