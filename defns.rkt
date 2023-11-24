bdec62e043ed7afe3b81df17c450d1063729549c
D. Ben Knoble <ben.knoble+github@gmail.com>
2023-11-24 17:12:26 -0500

    repair defns deserialization
    The refactoring in ceb011b (defns: extract new modules, 2023-11-18) did
    not reprovide deserialization information for structs declared with
    serializable-struct (because that information is only automatically
    provided in a submodule).
    
    Now that I've repaired this, it seems odd that manager.rkt should work
    to deserialize state values (since it doesn't include the submodules for
    serializable-structs in manager/*.rkt).

--- defns.rkt
+++ defns.rkt
@@ -2,6 +2,9 @@
 
 frosthaven-manager/defns/level
 frosthaven-manager/defns/loot
+(submod frosthaven-manager/defns/loot deserialize-info)
 frosthaven-manager/defns/monsters
+(submod frosthaven-manager/defns/monsters deserialize-info)
 frosthaven-manager/defns/players
+(submod frosthaven-manager/defns/players deserialize-info)
 frosthaven-manager/defns/scenario
