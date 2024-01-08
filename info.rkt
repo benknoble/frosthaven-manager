e601564d37c29f77c16e467f5c3343b208d5b76b
D. Ben Knoble <ben.knoble+github@gmail.com>
2024-01-08 11:16:34 -0500

    update missing deps
    Commit 29bf2e2 (create gui/rich-text-display, 2024-01-06) introduced a
    dependency but did not mark it.

--- info.rkt
+++ info.rkt
@@ -5,7 +5,8 @@
 (define racket-launcher-libraries '("gui/manager"))
 (define gracket-launcher-names '("FrosthavenManager"))
 (define gracket-launcher-libraries '("gui/manager"))
-(define deps '("pretty-expressive"
+(define deps '("pict-snip-lib"
+               "pretty-expressive"
                "alexis-multicast"
                "nat-traversal"
                "web-server-lib"
