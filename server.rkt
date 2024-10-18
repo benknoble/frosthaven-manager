benknoble/frosthaven-manager

commit aa1c03030aec6f70f231b9e05a513e993ddae718

fix: make SSE robust to missing abilities
A recent play session saw the following error:

    hash-ref: no value found for key
      key: "Ancient Artillery"
      context...:
       /Applications/Racket v8.14/collects/racket/private/list.rkt:405:36
       /Users/Knoble/code/frosthaven-manager/server.rkt:810:2
       /Users/Knoble/code/frosthaven-manager/server.rkt:802:4
       /Applications/Racket v8.14/share/pkgs/web-server-lib/web-server/http/response.rkt:179:7

There were some confusing issues during the game yesterday, so I'm not
sure exactly what caused this, but fortunately the stack trace was
clear. Like d7c7bd3 (fix: handle missing ability-decks gracefully,
2024-09-06), add defaults to the hash-ref call. Fortunately, all uses of
`ability` are already robust to #f as a possible value (because that is
how I typically represent the ability in the interstitial space between
rounds).

server.rkt
@@ -848,7 +848,7 @@
      (define mg (monster-group*-mg (creature-v c)))
      (define id (creature-id c))
      (define css-id (monster-group-css-id id))
-     (define ability (~>> (mg) monster-group-set-name (hash-ref ads) ability-decks-current))
+     (define ability (~>> (mg) monster-group-set-name (hash-ref ads #f) (and _ ability-decks-current)))
      (define data
        (hash 'id css-id
              'data (hash

commit d15f5f4344e825da6186a77bfbc8869db0ddc330

fix: get monster ability in server correctly
Commit aa1c030 (fix: make SSE robust to missing abilities, 2024-10-11)
broke the server refresh because it passed the monster group name as the
default value to hash-ref (with #f passed as the key to lookup):
compare the different threading operators in this commit. This commit
works, as evidenced by actual play (save & restore has never felt so
good).

server.rkt
@@ -863,7 +863,7 @@
      (define mg (monster-group*-mg (creature-v c)))
      (define id (creature-id c))
      (define css-id (monster-group-css-id id))
-     (define ability (~>> (mg) monster-group-set-name (hash-ref ads #f) (and _ ability-decks-current)))
+     (define ability (~> (mg) monster-group-set-name (hash-ref ads _ #f) (and _ ability-decks-current)))
      (define data
        (hash 'id css-id
              'data (hash