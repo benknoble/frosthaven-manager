benknoble/frosthaven-manager

commit f15c6fb4437501c5cbfd6e32bf0fe0356a2d1e70

validate bestiary formulas at parse time
This gives an earlier warning about incorrect formulas (at write/load
time rather than play time), making the cost of fixing them lower.

Close: https://github.com/benknoble/frosthaven-manager/issues/75

testfiles/parsers/monster.rkt
@@ -1,0 +1,47 @@
+#lang racket
+
+(require frosthaven-manager/parsers/monster
+         megaparsack
+         rackunit)
+
+(define bestiary-bad-formula
+  #<<EOF
+#lang frosthaven-manager/bestiary
+
+begin-monster "giant squid" ("boss")
+  [0 normal [HP "(C * 10"] [Move 3] [Attack     "C"]   ]
+  [0 elite  [HP "C * 10"] [Move 3] [Attack     "C"]   ]
+  [1 normal [HP "C * 12"] [Move 3] [Attack "C + 1"]   ]
+  [1 elite  [HP "C * 12"] [Move 3] [Attack "C + 1"]   ]
+  [2 normal [HP "C * 15"] [Move 3] [Attack "C + 1"]   ]
+  [2 elite  [HP "C * 15"] [Move 3] [Attack "C + 1"]   ]
+  [3 normal [HP "C * 17"] [Move 3] [Attack "C + 2"]   ]
+  [3 elite  [HP "C * 17"] [Move 3] [Attack "C + 2"]   ]
+  [4 normal [HP "C * 22"] [Move 3] [Attack "C + 2"]   ]
+  [4 elite  [HP "C * 22"] [Move 3] [Attack "C + 2"]   ]
+  [5 normal [HP "C * 25"] [Move 3] [Attack "C + 3"]   ]
+  [5 elite  [HP "C * 25"] [Move 3] [Attack "C + 3"]   ]
+  [6 normal [HP "C * 35"] [Move 3] [Attack "C + 4"]   ]
+  [6 elite  [HP "C * 35"] [Move 3] [Attack "C + 4"]   ]
+  [7 normal [HP "C * 35"] [Move 3] [Attack "C + 5"]   ]
+  [7 elite  [HP "C * 35"] [Move 3] [Attack "C + 5"]   ]
+end-monster
+
+begin-ability-deck "boss"
+  ["I'm a boss" 45 {"move +1" "attack -1 aoe(aoes/ring1.rkt)"}]
+  ["I'm a boss" 45 {"move +1" "attack -1 aoe(aoes/ring1.rkt)"}]
+  ["I'm a boss" 45 {"move +1" "attack -1 aoe(aoes/ring1.rkt)"}]
+  ["I'm a boss" 45 {"move +1" "attack -1 aoe(aoes/ring1.rkt)"}]
+  ["I'm a boss" 45 {"move +1" "attack -1 aoe(aoes/ring1.rkt)"}]
+  ["I'm a boss" 45 {"move +1" "attack -1 aoe(aoes/ring1.rkt)"}]
+  ["I'm a boss" 45 {"move +1" "attack -1 aoe(aoes/ring1.rkt)"}]
+  ["Special" 11 shuffle {"move +1" "attack +3"}]
+end-ability-deck
+EOF
+  )
+
+(check-exn exn:fail:read:megaparsack?
+           (thunk
+            (parse-bestiary 'bestiary-bad-formula
+                            (open-input-string bestiary-bad-formula)
+                            #:syntax? #f)))

parsers/monster.rkt
@@ -14,7 +14,8 @@
                                                (or/c #f (listof string?))))]))
 
 (require frosthaven-manager/defns
-         frosthaven-manager/parsers/base)
+         frosthaven-manager/parsers/base
+         frosthaven-manager/parsers/formula)
 
 (module+ test (require rackunit))
 
@@ -47,12 +48,22 @@
 (define monster-type/p
   (or/p (string-ci/p "normal") (string-ci/p "elite")))
 
+(define (check-formula str)
+  (with-handlers ([exn:fail:read:megaparsack? {#f}])
+    (parse-expr str)
+    #t))
+
+(define (valid-formula/p message)
+  (guard/p (non-empty-text/p message)
+           check-formula
+           "valid formula"))
+
 (define (value/p s p)
   (labelled/p (string-ci/p s) p))
 
 (define hp/p
   (value/p "HP"
-           (or/p (non-empty-text/p "non-empty HP formula")
+           (or/p (valid-formula/p "non-empty HP formula")
                  (guard/p number/p positive-integer? "positive maximum health"))))
 (define move/p
   (value/p "Move"
@@ -60,7 +71,7 @@
                  (guard/p number/p natural-number/c "base move value at least 0"))))
 (define attack/p
   (value/p "Attack"
-           (or/p (non-empty-text/p "non-empty Attack formula")
+           (or/p (valid-formula/p "non-empty Attack formula")
                  (guard/p number/p natural-number/c "base attack at least 0"))))
 (define bonuses/p (value/p "Bonuses" (list-value/p (non-empty-text/p "non-empty bonus text"))))
 (define effects/p (value/p "Effects" (list-value/p (non-empty-text/p "non-empty effect text"))))

commit 982ca60ee2d0c2cb4f80b748798c5a9b459a546b

build: fix test dependencies
Commit f15c6fb (validate bestiary formulas at parse time, 2024-10-27)
accidentally added what looks like a runtime dependency on rackunit-lib,
but it's intended as a test script. Move the test to a submodule to
avoid dependency issues.

Sidenote: CI has been failing for a month and I didn't notice?

testfiles/parsers/monster.rkt
@@ -1,8 +1,7 @@
 #lang racket
 
 (require frosthaven-manager/parsers/monster
-         megaparsack
-         rackunit)
+         megaparsack)
 
 (define bestiary-bad-formula
   #<<EOF
@@ -40,8 +39,10 @@ end-ability-deck
 EOF
   )
 
-(check-exn exn:fail:read:megaparsack?
-           (thunk
-            (parse-bestiary 'bestiary-bad-formula
-                            (open-input-string bestiary-bad-formula)
-                            #:syntax? #f)))
+(module+ test
+  (require rackunit)
+  (check-exn exn:fail:read:megaparsack?
+             (thunk
+              (parse-bestiary 'bestiary-bad-formula
+                              (open-input-string bestiary-bad-formula)
+                              #:syntax? #f))))