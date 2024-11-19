761f6987aef1b6fe95cea1757d7332c1635907a3
D. Ben Knoble <ben.knoble+github@gmail.com>
2024-11-19 14:53:21 -0500

    v0.23

--- info.rkt
+++ info.rkt
@@ -38,7 +38,7 @@
                      "racket-doc"))
 (define scribblings '(("scribblings/frosthaven-manager.scrbl" (multi-page))))
 (define pkg-desc "Frosthaven Scenario Manager")
-(define version "0.22")
+(define version "0.23")
 (define pkg-authors '(benknoble))
 (define license 'MIT)
 
